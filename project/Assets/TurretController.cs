da85100debc10d13846256f70cad7b3ead8a6b13
PYLEINNOVATIONS <danpyle@sbcglobal.net>
2015-06-10 19:11:39 -0400

    Initial commit
    First one

---
+++ project/Assets/TurretController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+//This code below is misplaced, might be useful for enemy missles
+/* public class ExampleClass : MonoBehaviour {
+public Transform prefab;
+void Start() {
+	for (int i = 0; i < 10; i++) {
+		Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+		i++;
+	} 
+}
+}*/
